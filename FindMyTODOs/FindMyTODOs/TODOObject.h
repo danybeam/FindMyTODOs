48fc0d957dabc6ce886938d69cc20f1b346f106e
Daniel Orozco <danybeam@gmail.com>
2024-04-09 23:09:55 -0400

    remove TODO comments

--- FindMyTODOs/FindMyTODOs/TODOObject.h
+++ FindMyTODOs/FindMyTODOs/TODOObject.h
@@ -3,25 +3,20 @@
 #include <string>
 #include <vector>
 
-// TODO: change structure to a N-Tree
-// TODO: Each node is aware of their level of indentation
 // TODO: Each node is in charge of printing only themselves with what they know
-// TODO: insert operation is assumed to be performed in the root node
-// TODO: needs to be aware of how deep we've gone to find its place
-class TODOObject
-{
+class TODOObject {
 public:
-	TODOObject(std::string name, bool isDirectory);
-	~TODOObject();
+  TODOObject(std::string name, bool isDirectory);
+  ~TODOObject();
 
-	void insertSubdirectory(std::string directoryPath);
-	void insertLine(std::string filePath, std::string line, int lineNumber);
-	std::string toString(int indentLevel);
+  void insertSubdirectory(std::string directoryPath);
+  void insertLine(std::string filePath, std::string line, int lineNumber);
+  std::string toString(int indentLevel);
 
 private:
-	bool isDirectory;
-	std::string name;
-	std::vector<std::string> lines;
-	std::vector<TODOObject*> childrenDirectories;
-	std::vector<TODOObject*> files;
+  bool isDirectory;
+  std::string name;
+  std::vector<std::string> lines;
+  std::vector<TODOObject *> childrenDirectories;
+  std::vector<TODOObject *> files;
 };
