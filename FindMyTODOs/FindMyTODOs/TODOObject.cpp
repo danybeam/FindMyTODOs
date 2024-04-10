b4c53483583ca8b60cbc07b119c12e0566fa3d3a
Daniel Orozco <danybeam@gmail.com>
2024-04-10 17:19:09 -0400

    finish case with multiple siblings

--- FindMyTODOs/FindMyTODOs/TODOObject.cpp
+++ FindMyTODOs/FindMyTODOs/TODOObject.cpp
@@ -47,19 +47,24 @@ void TODOObject::insertLine(std::string filePath, std::string line, int lineNumb
 	{
 		if (filePath.find(file->name) == 0)
 		{
-			file->lines.emplace_back("(" + std::to_string(lineNumber) + ")\t: " + line);
+			file->lines.emplace_back("(L" + std::to_string(lineNumber) + ")\t: " + line);
 			return;
 		}
 	}
 
 	this->files.push_back(new TODOObject(filePath, false));
-	this->files.back()->lines.emplace_back("(" + std::to_string(lineNumber) + ")\t: " + line);
+	this->files.back()->lines.emplace_back("(L" + std::to_string(lineNumber) + ")\t: " + line);
 }
 
 std::string TODOObject::toString(int indentLevel, int lastItemCount)
 {
 	std::stringstream buffer;
 	int localLast = lastItemCount;
+	bool imLast = localLast < 0;
+	if (imLast)
+	{
+		localLast *= -1;
+	}
 	bool atLeastOne = false;
 
 	if (this->childrenDirectories.empty() && this->files.empty() && this->lines.empty())
@@ -67,12 +72,65 @@ std::string TODOObject::toString(int indentLevel, int lastItemCount)
 		return std::string();
 	}
 
+	// Tree stems
+	for (int i = 1; i < indentLevel; i++)
+	{
+		char head = '|';
+		if (
+			indentLevel == localLast ||
+			(localLast > 1 && i >= localLast)
+			)
+		{
+			head = ' ';
+		}
+		buffer << head << "  ";
+	}
+
+	// arrow
+	char head = (unsigned char)194;
+	if (indentLevel == 0)
+	{
+		head = 218;
+	}
+	else
+	{
+		unsigned char subHead = 195;
+		if (imLast)
+		{
+			subHead = 192;
+		}
+		buffer << subHead << "--";
+	}
+	buffer << head << "->";
+
+
 	// File/directory name
-	buffer << indentLevel << ", " << localLast << " " << this->name << "\n";
+	buffer << this->name << "\n";
 
 	indentLevel++;
+
 	for (std::string& line : this->lines)
 	{
+		// Tree stems
+		for (int i = 1; i < indentLevel; i++)
+		{
+			char head = '|';
+			if (i >= (indentLevel - localLast))
+			{
+				head = ' ';
+			}
+			buffer << head << "  ";
+		}
+
+		// arrow
+		unsigned char head = 195;
+		if (line == this->lines.back())
+		{
+			head = 192;
+		}
+		buffer << head << "---->";
+
+
 		buffer << line << "\n";
 		atLeastOne |= true;
 	}
@@ -82,6 +140,7 @@ std::string TODOObject::toString(int indentLevel, int lastItemCount)
 		if (subItem == this->files.back() && this->childrenDirectories.empty())
 		{
 			localLast++;
+			localLast *= -1;
 		}
 
 		std::string temp = subItem->toString(indentLevel, localLast);
@@ -89,11 +148,18 @@ std::string TODOObject::toString(int indentLevel, int lastItemCount)
 		buffer << temp;
 	}
 
+	localLast = lastItemCount;
+	if (imLast)
+	{
+		localLast *= -1;
+	}
+
 	for (TODOObject* subItem : this->childrenDirectories)
 	{
 		if (subItem == this->childrenDirectories.back())
 		{
 			localLast++;
+			localLast *= -1;
 		}
 
 		std::string temp = subItem->toString(indentLevel, localLast);
